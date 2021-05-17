83f67631ded3e712d2496c040b96ed204bdd7ec1
lentrix <hawkmanlentrix@gmail.com>
2021-05-17 15:01:07 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="row">
+    <div class="col-md-4">
+      <h1>Titles</h1>
+      <div class="titles">
+          <ul class="list-group">
+          <li class="list-group-item" v-for="post in posts" :key="post.id">
+            <a href="#" class="list-group-item-action"
+                @click.prevent="selectPost(post)">
+              {{post.title}}
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="col-md-8">
+      <Post :post="selectedPost" v-if="selectedPost!=null" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import Post from './components/Post.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Post
+  },
+  data() {
+    return {
+      posts: [],
+      selectedPost: null
+    }
+  },
+  methods: {
+    async getPosts() {
+      await axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        if(response.status==200) {
+          this.posts = response.data;
+        }
+      })
+    },
+    selectPost(post) {
+      this.selectedPost = post
+    }
+  },
+  created() {
+    this.getPosts()
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.titles {
+  height: 88vh;
+  overflow: scroll;
 }
 </style>
---
+++ src/components/Post.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-if="post!=null">
+            <h1>{{post.title}}</h1>
+            <div>
+                {{post.body}}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        post: null
+    }
+}
+</script>
+
+<style>
+
+</style>
